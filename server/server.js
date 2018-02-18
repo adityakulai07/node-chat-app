3c9c8ec4d839a9fd5f68fceff290fb78f5b95378
Aditya Kulai <adityakulai93@gmail.com>
2018-02-17 19:46:29 -0500

    Greet new users and alert others

--- server/server.js
+++ server/server.js
@@ -15,20 +15,39 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user is connected');
 
+  //for new connection(user joined)
+  //socket.emit from Admin text Welcome to chat room(to the new user)
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to chat room',
+    createdAt: new Date().getTime()
+  });
+  //socket.broadcast.emit from Admin text new user has joined(to everyone else in the chat room)
+  socket.broadcast.emit('newMessage',{
+    from: 'Admin',
+    text: 'New user has joined',
+    createdAt: new Date().getTime()
+  });
   //Listener(chat app)
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from:  message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+
   });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
-
-  //Custom events
 
 });
 
